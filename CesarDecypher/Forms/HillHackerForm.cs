db06a1201c6e4673d01c085c5b080bcd14f2a6d5
Кубко Юлия <jtesseract13f@gmail.com>
2024-11-09 22:18:44 +0600

    1100 1100

--- CesarDecypher/Forms/HillHackerForm.cs
+++ CesarDecypher/Forms/HillHackerForm.cs
@@ -33,6 +33,7 @@ namespace CesarDecypher.Forms
             };
             buttons = new Dictionary<Button, List<TextBox>>();
             buttons[button2] = parts[0];
+            button2.Click += new EventHandler(deletePairButton);
             groupBox1.Controls.Add(button2);
             groupBox1.Controls.Add(parts[0][0]);
             groupBox1.Controls.Add(parts[0][1]);
@@ -66,22 +67,57 @@ namespace CesarDecypher.Forms
         {
             var button = new Button()
             {
-                Location = new Point(261, 51 + buttons.Count * textBoxHeight),
                 Width = 22,
                 Height = 21,
-                Text = "--"
+                Text = "--",
+                Location = new Point(261, 51 + buttons.Count * textBoxHeight)
             };
             var textBoxes = new List<TextBox>()
             {
-                new TextBox() { Location = new Point(6, 51 + buttons.Count * textBoxHeight), Width = 120},
-                new TextBox(){ Location = new Point(135, 51 + buttons.Count * textBoxHeight), Width = 120}
+                new TextBox() { Width = 120, Location = new Point(6, 51 + buttons.Count * textBoxHeight)},
+                new TextBox(){ Width = 120, Location = new Point(135, 51 + buttons.Count * textBoxHeight)}
             };
             groupBox1.Controls.Add(button);
             groupBox1.Controls.Add(textBoxes[0]);
             groupBox1.Controls.Add(textBoxes[1]);
+            button.Click += new EventHandler(deletePairButton);
             buttons[button] = textBoxes;
             parts.Add(textBoxes);
-            addPairButton.Location = new Point(6, 88 + buttons.Count * textBoxHeight);
+            addPairButton.Location = new Point(6, 88 + (buttons.Count-1) * textBoxHeight);
+            FormateTextBoxes();
+        }
+
+        private void deletePairButton(object sender, EventArgs e)
+        {
+            if (!(sender is Button)) {
+                return;
+            }
+            var button = (Button)sender;
+            var textBoxes = buttons[button];
+            groupBox1.Controls.Remove(button);
+            groupBox1.Controls.Remove(textBoxes[0]);
+            groupBox1.Controls.Remove(textBoxes[1]);
+            parts.Remove(textBoxes);
+            buttons.Remove(button);
+            button.Dispose();
+            textBoxes.ForEach(x => x.Dispose());
+            FormateTextBoxes();
+        }
+
+        void FormateTextBoxes()
+        {
+            var l = buttons.ToList();
+            for (int i = 0; i < l.Count; ++i)
+            {
+                var button = l[i].Key;
+                var textBoxes = l[i].Value;
+
+                button.Location = new Point(261, 51 + i * textBoxHeight);
+                textBoxes[0].Location = new Point(6, 51 + i * textBoxHeight);
+                textBoxes[1].Location = new Point(135, 51 + i * textBoxHeight);
+            }
+
+            addPairButton.Location = new Point(6, 88 + (buttons.Count - 1) * textBoxHeight);
         }
     }
 }
